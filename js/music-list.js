8a67769e01b3f5916964fbb9959c72284813756f
renzycode <renzyjohnm1@gmail.com>
2023-10-11 22:47:22 +0800

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,247 @@
+let allMusic = [
+
+    {
+        name: "Paraluman",
+        artist: "Adie",
+        img: "music-1",
+        src: "music-1",
+        lyrics: `
+        Sa unang tingin, agad na nahumaling
+        Sa nagniningning mong mga mata
+        Ika'y isang bituin na nagmula sa langit
+
+        Hindi ko mawari ang taglay mong tinatangi
+        Sadya namang nakakabighani
+        'Di maipaliwanag ang nararamdaman
+
+        Namumukadkad ang aking ligaya
+        Sa tuwing ika'y papalapit na
+        Hawakan mo ang aking kamay
+
+        Oh, Paraluman, ika'y akin nang
+        Dadalhin sa 'di mo inaasahang paraiso
+        Palagi kitang aawitan ng Kundiman
+        'Di magsasawa, 'di ka pababayaan
+        Isasayaw kita hanggang sa walang-hanggan
+
+        Mga gunita na laging naiisip (naiisip)
+        Sumisilip (sumisilip) ang itinakda ng mahiwaga
+        Liwanag na dulot mo, nagbigay-sinag sa madilim kong mundo
+        Ibang-iba ako kapag ikaw na ang kapiling
+        Sumisiping ang buwan at mga bituin
+        Na para bang sumasang-ayon sa atin ang kalawakan (kalawakan)
+
+        Namumukadkad ang aking ligaya
+        Sa tuwing ika'y papalapit na
+        Hawakan mo ang aking kamay
+
+        Oh, Paraluman, ika'y akin nang
+        Dadalhin sa 'di mo inaasahang paraiso (paraiso)
+        Palagi kitang aawitan ng Kundiman
+        'Di magsasawa, 'di ka pababayaan
+        Isasayaw kita hanggang sa walang-hanggan
+
+        Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman
+        Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman
+        Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman
+        Pa-pa-para-pa-pa, para-pa-pa-para-Paraluman
+
+        Himig ng tadhana
+        Sa atin ay tumutugma na
+        Himig ng tadhana
+        Sa atin ay tumutugma na
+        Himig ng tadhana
+        Sa atin ay tumutugma na
+
+        Oh, Paraluman, ika'y akin nang
+        Dadalhin sa 'di mo inaasahang paraiso
+        Palagi kitang aawitan ng Kundiman
+        'Di magsasawa, 'di ka pababayaan
+        Isasayaw kita, mamahalin kita
+        Hanggang sa walang-hanggan
+        `
+    },
+    {
+        name: "Ligaya",
+        artist: "Mrld",
+        img: "music-2",
+        src: "music-2",
+        lyrics: `
+        Sa 'kin nakatingin, parang lumilipad na lang sa hangin
+        'Di na mapakali, 'kaw lang naman ang ninanais
+        'Di ba nalilito ang puso mo kung sa'n na papunta?
+        Sa akin ba, sinta?
+
+        Huwag mo sanang isipin na parang wala lang sa 'kin
+        Ang lahat nating pinagsamahan
+
+        Isasayaw kita na para bang
+        Ito na ang panghuli natin, sinta
+        'Wag kang mag-alala, 'di kita hahayaang mag-isa
+        Sasamahan nga kita sa dulo ng ligaya
+
+        'Di pa ba halata na iba ang ihip ng hangin dito?
+        Kung pwede lang sabihin nang deretsahan ang aking gusto
+        At teka, ang bulag mo naman kung 'di pa rin nasisilayan
+        Nagbago na ang pagtingin
+
+        Huwag mo sanang isipin na parang wala lang sa 'kin
+        Ang lahat nating pinagsamahan
+
+        Isasayaw kita na para bang
+        Ito na ang panghuli natin, sinta
+        'Wag kang mag-alala, 'di kita hahayaang mag-isa
+        Sasamahan nga kita sa dulo ng ligaya
+
+        At ang oras ay tumatakbo
+        Hindi pa rin alam kung gusto mo rin ako
+
+        Isasayaw kita na para bang
+        Walang nakatingin sa 'tin, sinta
+        'Wag kang mag-alala, 'di kita hahayaang mag-isa
+        Sasamahan nga kita...
+        
+        Isasayaw kita na para bang
+        Ito na ang panghuli natin, sinta
+        'Wag kang mag-alala, 'di kita hahayaang mag-isa
+        Sasamahan nga kita sa dulo...
+        `
+    },
+    {
+        name: "Nangangamba",
+        artist: "Zack Tabudlo",
+        img: "music-3",
+        src: "music-3",
+        lyrics: `
+        Nangangamba nangangamba ang 'yong puso
+        Hindi ka sigurado ('di ka sigurado)
+        Nalilito nalilito ang 'yong utak
+        Kung tunay bang pag ibig 'to (tunay bang pag ibig 'to)
+
+        Ano ba ang problema mo
+        Sabihin na ang totoo
+
+        Sabihin mo na nilalaman ng puso mo
+        At nararamdaman nito
+        Kung 'di pa aminin ang gusto
+        Baka kasi mawala na ako
+
+        Nagulat ka nagulat ka no'ng
+        May kasama na 'kong iba (kasama na 'kong iba)
+        Pakipot ka 'wag nang pakipot pa
+        Baka maagaw pa ng iba (maagaw pa ng iba)
+
+        Wala nang magbabago
+        Kung 'di mo pa aminin 'to
+
+        Sabihin mo na nilalaman ng puso mo
+        At nararamdaman nito
+        Kung 'di pa aminin ang gusto
+        Baka kasi mawala na ako
+
+        Ito na ang pagkakataon
+        Wala nang pipigil sa 'yo
+        'Wag ka nang mahihiya
+        Sabihin na ang totoo
+
+        Sabihin mo na nilalaman ng puso mo
+        At nararamdaman nito
+        Kung 'di pa aminin ang gusto oh
+
+        Sabihin mo na nilalaman ng puso mo
+        At nararamdaman nito
+        Kung 'di pa aminin ang gusto
+        Baka kasi mawala na ako
+        `
+    },
+    {
+        name: "Rainbow",
+        artist: "Southborder",
+        img: "music-4",
+        src: "music-4",
+        lyrics: `
+        Fallin' out, fallin' in
+        Nothing's sure in this world, no, no
+        Breakin' out, breakin' in
+        Never knowin' what lies ahead
+        We can really never tell it all, no, no, no
+
+        Say "Goodbye", say "Hello"
+        To a lover or friend
+        Sometimes, we never could understand
+        Why some things begin, then just end
+        We can really never tell it all, no, no, no
+
+        But, oh, can't you see
+        That no matter what happens, life goes on and on?
+        And so, oh, baby, just smile
+        'Cause I'm always around you
+        And I'll make you see how beautiful life is for you and me
+
+        Take a little time, baby
+        See the butterflies' colors
+        Listen to the birds that were sent
+        To sing for me and you
+        Can you feel me?
+        This is such a wonderful place to be
+
+        Even if there is pain now
+        Everything would be alright
+        For as long as the world still turns
+        There will be night and day
+        Can you hear me?
+        There's a rainbow always after the rain
+
+        Oh, whoa
+
+        Hittin' high, hittin' low
+        Win or lose, you should go, yeah, yeah
+        Gettin' warm, gettin' cold
+        Weather could be so good or bad
+        But, baby, this is life now, don't get mad, no, no, no
+
+        'Cause, oh, can't you see (can't you see?)
+        That no matter what happens, life goes on and on?
+        So, baby, please smile (just smile)
+        'Cause I'm always around you
+        And I'll make you see how beautiful life is for you and me
+
+        Take a little time, baby
+        See the butterflies' colors
+        Listen to the birds that were sent
+        To sing for me and you
+        Can you feel me?
+        This is such a wonderful place to be
+
+        Even if there is pain now
+        Everything would be alright
+        For as long as the world still turns
+        There will be night and day
+        Can you hear me?
+        There's a rainbow always after the rain
+
+        Life's full of challenges
+        Not all the time we get what we want
+        But don't despair, my dear (I know now)
+        You'll take each trial, and you'll make it through the storm
+        'Cause you're strong, my faith in you is clear
+        So, I'll say once again, this world's wonderful
+        And let us celebrate life that's so beautiful, so beautiful
+
+        Take a little time, baby
+        See the butterflies' colors
+        Listen to the birds that were sent
+        To sing for me and you
+        Can you feel me?
+        This is such a wonderful place to be
+
+        Even if there is pain now
+        Everything would be alright
+        For as long as the world still turns
+        There will be night and day
+        Can you hear me?
+        There's a rainbow always after the rain
+        `
+    },
+    
+];
---
+++ js/script.js
@@ -0,0 +1,275 @@
+const container = document.querySelector(".container"),
+    musicImg = container.querySelector(".img-area img"),
+    musicName = container.querySelector(".song-details .name"),
+    musicArtist = container.querySelector(".song-details .artist"),
+    mainAudio = container.querySelector("#main-audio"),
+    playpauseBtn = container.querySelector(".play-pause"),
+    nextBtn = container.querySelector("#next"),
+    prevBtn = container.querySelector("#prev"),
+    progressArea = container.querySelector(".progress-area"),
+    progressBar = container.querySelector(".progress-bar"),
+    musicList = container.querySelector(".music-list"),
+    moreMusicBtn = container.querySelector("#more-music"),
+    closemoreMusic = container.querySelector("#close"),
+    musicLyrics = document.querySelector(".music-lyrics"),
+    playSongInListBtn = document.querySelector(".playSongInListBtn"),
+    lyricsSongName = document.querySelector("#lyrics-song-name"),
+    lyricsSongArtist = document.querySelector("#lyrics-song-artist"),
+    renderMusicsHere = document.querySelector("#render-musics-here");
+
+
+let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+    playingSong();
+})
+
+for(let i = 0; i < allMusic.length; i++){
+    renderMusicsHere.innerHTML += `
+    <li>
+        <div class="list-song">
+            <div class="img-area-list">
+            <img src="images\\${allMusic[i].img}.jpg" alt="img">
+            </div>
+            <svg class="play-button playSongInListBtn" songname="${allMusic[i].src}" xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+            </svg>
+        </div>
+        <div class="list-song song-list-details">
+        <p>${allMusic[i].name}</p>
+        <p class="list-artist">${allMusic[i].artist}</p>
+        </div>
+    </li>
+    `
+}
+
+function loadMusic(indexNumb) {
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+    musicLyrics.innerText = allMusic[indexNumb - 1].lyrics;
+    lyricsSongName.innerText = allMusic[indexNumb - 1].name;
+    lyricsSongArtist.innerText = `song by ${allMusic[indexNumb - 1].artist}`;
+}
+
+function playMusic() {
+    container.classList.add("paused");
+    playpauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+
+function pauseMusic() {
+    container.classList.remove("paused");
+    playpauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause();
+}
+
+function nextMusic() {
+    musicIndex++; 
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong();
+}
+
+function prevMusic() {
+    musicIndex--; 
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong();
+}
+
+playpauseBtn.addEventListener("click", () => {
+    const isMusicPaused = container.classList.contains("paused");
+
+    isMusicPaused ? pauseMusic() : playMusic();
+
+});
+
+
+document.querySelectorAll('.playSongInListBtn').forEach((li) => {
+    li.addEventListener('click', (event) => {
+        let selected_song = li.getAttribute('songname');
+        console.log(selected_song);
+        for(let i = 0; i < allMusic.length; i++){
+            if(selected_song == allMusic[i].src){
+                loadMusic(i+1);
+                playMusic();
+                playingSong();
+            }
+        }
+    });
+});
+
+nextBtn.addEventListener("click", () => {
+    nextMusic();
+});
+
+prevBtn.addEventListener("click", () => {
+    prevMusic();
+});
+
+
+mainAudio.addEventListener("timeupdate", (e) => {
+    const currentTime = e.target.currentTime; 
+    const duration = e.target.duration; 
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+
+
+
+    let musicCurrentTime = container.querySelector(".current-time"),
+        musicDuration = container.querySelector(".max-duration");
+    mainAudio.addEventListener("loadeddata", () => {
+
+        
+        let mainAdDuration = mainAudio.duration;
+        let totalMin = Math.floor(mainAdDuration / 60);
+        let totalSec = Math.floor(mainAdDuration % 60);
+        if (totalSec < 10) { //if sec is less than 10 then add 0 before it
+            totalSec = `0${totalSec}`;
+        }
+
+        musicDuration.innerText = `${totalMin}:${totalSec}`;
+
+    });
+
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if (currentSec < 10) { 
+        currentSec = `0${currentSec}`;
+    }
+
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+
+});
+
+progressArea.addEventListener("click", (e) => {
+    let progressWidth = progressArea.clientWidth; 
+    let clickedOffsetX = e.offsetX; 
+    let songDuration = mainAudio.duration; 
+
+    mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
+    playMusic();
+
+});
+
+
+const repeatBtn = container.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", () => {
+    let getText = repeatBtn.innerText; 
+    switch (getText) {
+        case "repeat":
+            repeatBtn.innerText = "repeat_one";
+            repeatBtn.setAttribute("title", "song looped");
+            break;
+        case "repeat_one":
+            repeatBtn.innerText = "shuffle";
+            repeatBtn.setAttribute("title", "playback shuffled");
+            break;
+        case "shuffle":
+            repeatBtn.innerText = "repeat";
+            repeatBtn.setAttribute("title", "playlist looped");
+            break;
+
+    }
+
+});
+
+
+mainAudio.addEventListener("ended", () => {
+    let getText = repeatBtn.innerText; 
+    switch (getText) {
+        case "repeat":
+            nextMusic(); 
+            break;
+        case "repeat_one":
+            mainAudio.currentTime = 0; 
+            loadMusic(musicIndex); 
+            playMusic(); 
+            break;
+        case "shuffle":
+            let randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+            do {
+                randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+            } while (musicIndex == randIndex); 
+            musicIndex = randIndex; 
+            loadMusic(musicIndex);
+            playMusic();
+            playingSong();
+            break;
+    }
+});
+
+moreMusicBtn.addEventListener("click", () => {
+    musicList.classList.toggle("show");
+});
+closemoreMusic.addEventListener("click", () => {
+    moreMusicBtn.click();
+});
+
+
+const ulTag = container.querySelector("ul");
+
+for (let i = 0; i < allMusic.length; i++) {
+    let liTag = `<li li-index="${i + 1}">
+    <div class="row">
+      <span>${allMusic[i].name}</span>
+      <p>${allMusic[i].artist}</p>
+    </div>
+    <audio class="${allMusic[i].src} " src="songs/${allMusic[i].src}.mp3"></audio>
+    <span id="${allMusic[i].src}" class="audio-duration">1:45</span>
+  </li>`;
+  ulTag.insertAdjacentHTML("beforeend", liTag);
+
+  let liAudioDurationTag = ulTag.querySelector(`#${allMusic[i].src}`);
+  let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`);
+
+  liAudioTag.addEventListener("loadeddata", () => {
+    let duration = liAudioTag.duration;
+    let totalMin = Math.floor(duration / 60);
+    let totalSec = Math.floor(duration % 60);
+    if (totalSec < 10) { //if sec is less than 10 then add 0 before it
+        totalSec = `0${totalSec}`;
+    }
+
+    liAudioDurationTag.innerText = `${totalMin}:${totalSec}`;
+    liAudioDurationTag.setAttribute("t-duration", `${totalMin}:${totalSec}`);
+  });
+
+}
+
+const allLiTags = ulTag.querySelectorAll("li");
+function playingSong() {
+    for (let j = 0; j < allLiTags.length; j++) {
+   let audioTag = allLiTags[j].querySelector(".audio-duration");
+        if(allLiTags[j].classList.contains("playing")){
+            allLiTags[j].classList.remove("playing");
+        
+        let adDuration = audioTag.getAttribute("t-duration");
+        audioTag.innerText = adDuration;
+        }
+    
+        if(allLiTags[j].getAttribute("li-index") == musicIndex){
+            allLiTags[j].classList.add("playing");
+            audioTag.innerText = "Playing";
+        }
+    
+        allLiTags[j].setAttribute("onclick", "clicked(this)");
+    }
+}
+
+
+function clicked(element){
+
+    
+    let getLiIndex = element.getAttribute("li-index");
+    musicIndex =  getLiIndex; 
+    loadMusic(musicIndex);
+    playMusic(); 
+    playingSong();
+   
+}
